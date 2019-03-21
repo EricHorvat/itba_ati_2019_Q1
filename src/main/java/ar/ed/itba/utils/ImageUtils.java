3aee95f59d205ead9af116e189d936526f06d571
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-03-21 00:54:08 -0300

    [ADD] Sum and Less rgb values

--- src/main/java/ar/ed/itba/utils/ImageUtils.java
+++ src/main/java/ar/ed/itba/utils/ImageUtils.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
 
 public final class ImageUtils {
 	
@@ -21,4 +25,21 @@ public final class ImageUtils {
 		c = new Color(255-c.getRed(),255-c.getBlue(),255-c.getGreen());
 		return c.getRGB();
 	}
+	
+	public static int sumRGB(Collection<Integer> rgbs, int min){
+		Optional<Integer> result = rgbs.stream().reduce((int1, int2) -> int1 + int2);
+		return result.map(integer -> (integer - min)/(rgbs.size() - min)).orElse(0);
+	}
+	
+	public static int sumRGB(Collection<Integer> rgbs){
+		return sumRGB(rgbs, 0);
+	}
+	
+	public static int lessRGB(Integer rgb1, Integer rgb2){
+		ArrayList<Integer>  a = new ArrayList<>();
+		a.add(rgb1);
+		a.add(rgb2 - 255);
+		return sumRGB(a, -255);
+	}
+	
 }
