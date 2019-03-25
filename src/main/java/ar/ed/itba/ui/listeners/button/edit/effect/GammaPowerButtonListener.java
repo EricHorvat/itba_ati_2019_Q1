774e13de41695b71806851820a1ecce1b16bdcb4
mperazzo <maperazzo@itba.edu.ar>
2019-03-25 17:51:20 -0300

    Add UI gamma power button

--- src/main/java/ar/ed/itba/ui/listeners/button/edit/effect/GammaPowerButtonListener.java
+++ src/main/java/ar/ed/itba/ui/listeners/button/edit/effect/GammaPowerButtonListener.java
@@ -1,17 +1,28 @@
 package ar.ed.itba.ui.listeners.button.edit.effect;
 
 import ar.ed.itba.ui.components.DialogFactory;
+import ar.ed.itba.ui.frames.EditableImageFrame;
+import ar.ed.itba.ui.frames.OriginalImageFrame;
+import ar.ed.itba.utils.ImageUtils;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GammaPowerButtonListener implements ActionListener {
+
+	private final JTextField gammaField;
+
 	public GammaPowerButtonListener(JTextField gammaField) {
+		this.gammaField = gammaField;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent actionEvent) {
-		DialogFactory.notImplementedDialog();
+		final double gamma = Double.parseDouble(gammaField.getText());
+		EditableImageFrame editableImageFrame = EditableImageFrame.instance();
+		ImageUtils.gammaPower(editableImageFrame.getAtiImage(), gamma);
+		editableImageFrame.buildAndShow();
+
 	}
 }
