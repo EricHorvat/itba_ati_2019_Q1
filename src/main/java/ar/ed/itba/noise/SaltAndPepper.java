7d4c275faf6caee19f19e656ff2393ab39a0e7ac
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-03-28 21:23:35 -0300

    [ADD] Counter Salt and pepper applies

--- src/main/java/ar/ed/itba/noise/SaltAndPepper.java
+++ src/main/java/ar/ed/itba/noise/SaltAndPepper.java
@@ -11,21 +11,28 @@ public class SaltAndPepper {
             throw new IllegalArgumentException("P a probability [0,1]");
         
         final int[] pixels = image.toRGB();
+        int count = 0;
         for (int i = 0 ; i < image.getHeight() ; i++) {
             for (int j = 0 ; j < image.getWidth() ; j++) {
             	double x = Math.random();
              
-            	if (x < p) {
+            	// 0.05 > x
+            	if (p > x) {
             		pixels[(i * image.getWidth() + j) * 3] = 255;
             		pixels[(i * image.getWidth() + j) * 3 + 1] = 255;
             		pixels[(i * image.getWidth() + j) * 3 + 2] = 255;
+            		count++;
             	} else if (x > (1 - p)){
+            		// x > 0.95
             		pixels[(i * image.getWidth() + j) * 3] = 0;
             		pixels[(i * image.getWidth() + j) * 3 + 1] = 0;
             		pixels[(i * image.getWidth() + j) * 3 + 2] = 0;
+            		count++;
             	}
             }
         }
+        System.out.println("Salt&Pepper Noise applied over " + count+ " pixel, from an image of " +
+	        image.getWidth()*image.getHeight() + "pixels.");
         return new PpmImage(pixels, image.getWidth(), image.getHeight());
     }
 }
